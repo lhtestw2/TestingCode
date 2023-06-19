0aae35629779c209e4ef30bdd3627e7e4e8550a2
lhtestw2 <602870539@qq.com>
2023-06-19 10:49:39 +0800

    Add similarity

--- factory.cpp
+++ factory.cpp
@@ -2,6 +2,9 @@
  * Test about factory mode.
  */
 #include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
 
 #define TEST 1
 
@@ -31,12 +34,42 @@ class MoreTest {
 };
 
 
+int getEditDistance(std::string first, std::string second) {
+    int m = first.length();
+    int n = second.length();
+    std::vector<std::vector<size_t>> record(m + 1, std::vector<size_t>(n + 1, 0));
+    for (int i = 0; i <= m; ++i) {
+        record[i][0] = i;
+    }
+    for (int j = 0; j <= n; ++j) {
+        record[0][j] = j;
+    }
+    for (int i = 1; i <= m; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            size_t weight = first[i - 1] == second[j - 1] ? 0 : 1;
+            record[i][j] = std::min(std::min(record[i - 1][j] + 1, record[i][j - 1] + 1), 
+            record[i - 1][j - 1] + weight);
+        }
+    }
+    return record[m][n];
+}
+
+double StringSimilarity(std::string first, std::string second) {
+    double max_len = std::max(first.length(), second.length());
+    if (max_len > 0) {
+        return (max_len - getEditDistance(first, second)) / max_len;
+    }
+    return 1;
+}
+
+
 int main()
 {
-    ATest atest;
-    std::cout << atest.a << ' ' << atest.b << std::endl;
-    std::cout << 2 << std::endl;
-    BTest btest(1.2);
-    std::cout << btest.a << ' ' << btest.b << ' ' << btest.c << std::endl;
+    // ATest atest;
+    // std::cout << atest.a << ' ' << atest.b << std::endl;
+    // std::cout << 2 << std::endl;
+    // BTest btest(1.2);
+    // std::cout << btest.a << ' ' << btest.b << ' ' << btest.c << std::endl;
+    std::cout << StringSimilarity("test", "log") << std::endl;
     return 0;
 }
